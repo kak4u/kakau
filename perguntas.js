832851f6379eff2fe224414f832b6ef06064d6b7
kak4u <kauany.cruzara@escola.pr.gov.br>
2025-08-07 08:59:46 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+const perguntas = [
+{
+categoria: "historia",
+pergunta: "quando foi a descoberta do Brasil",
+resposta: "22 de maio de 1500"
+},
+{
+categoria: "geografia",
+pergunta: "qual a capital do Brasil",
+resposta: "Brasília"
+},
+{
+categoria: "fisica",
+pergunta: "quantos planetas tem no sistema solar",
+resposta: "8 planetas"
+},
+{
+categoria: "educação fisica",
+pergunta: "quantas pessoas precisam para jogar volei",
+resposta: "6 pessoas"
+},
+{
+categoria: "quimica",
+pergunta: "o que é um àtomo",
+resposta: "é a unidade basica de construção da matéria"
+];
+}
